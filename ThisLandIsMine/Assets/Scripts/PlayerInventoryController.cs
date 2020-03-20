a5ec436ea3097a1541abad740daf4744eb0006e1
Mathieu VIALES <mathieu@viales.fr>
2020-03-20 16:29:34 +0100

    Add crafting

--- ThisLandIsMine/Assets/Scripts/PlayerInventoryController.cs
+++ ThisLandIsMine/Assets/Scripts/PlayerInventoryController.cs
@@ -1,11 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 
 public class PlayerInventoryController : MonoBehaviour
 {    
     private Dictionary<ResourceType, int> Ressources { get; set; } = new Dictionary<ResourceType, int>();
     public PlayerUiController Ui { get; set; }
+    private List<Item> Inventory = new List<Item>();
+    private List<Item> CraftableItems = new List<Item>()
+    {
+        new WoodenThrowingKnife()
+    };
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CraftItem();
+            Ui.UpdateResources(Ressources);
+            Ui.UpdateInventory(Inventory,CraftableItems, Ressources);
+        }
+    }
+
+    public void CraftItem()
+    {
+        if (WoodenThrowingKnife.GetMaxNumberOfCraftsPossible(Ressources) > 0)
+        {
+            var createdCount = Item.Craft(Ressources);
+            var item = Inventory.SingleOrDefault(i => i.Id == 1);
+            if (item == null)
+            {
+                item = new WoodenThrowingKnife(createdCount);
+                Inventory.Add(item);
+            }
+            else
+            {
+                item.Count += createdCount;
+            }
+
+        }
+    }
 
     public int AddToResource(ResourceValue resourceValue)
     {
@@ -24,6 +61,7 @@ public class PlayerInventoryController : MonoBehaviour
         }
 
         Ui.UpdateResources(Ressources);
+        Ui.UpdateInventory(Inventory,CraftableItems, Ressources);
         return Ressources[type];
-    }
+    }   
 }
--- ThisLandIsMine/Assets/Scripts/PlayerUiController.cs
+++ ThisLandIsMine/Assets/Scripts/PlayerUiController.cs
@@ -9,10 +9,26 @@ using UnityEngine.UI;
 public class PlayerUiController : MonoBehaviour
 {
     private Text ResourcesText;
+    private Text InventoryText;
 
     public void Start()
     {
         ResourcesText = gameObject.transform.Find("ResourceCount").GetComponent<Text>();
+        InventoryText = gameObject.transform.Find("Inventory").GetComponent<Text>();
+    }
+
+    public void UpdateInventory(List<Item> inventory, List<Item> craftableItems, Dictionary<ResourceType, int> ressources)
+    {
+        var inventoryAndCrafts = new List<Item>(craftableItems);
+        inventoryAndCrafts.ForEach(c => { c.Count = inventory.SingleOrDefault(s => s.Id == c.Id)?.Count ?? 0; });
+        
+        var stringBuilder = new StringBuilder();
+        foreach (var item in inventoryAndCrafts)
+        {
+            stringBuilder.AppendLine($"{item.Name} - {item.Count} - {Item.GetMaxNumberOfCraftsPossible(ressources)}");
+        }
+
+        InventoryText.text = stringBuilder.ToString();
     }
 
     public void UpdateResources(Dictionary<ResourceType, int> resources)
@@ -32,5 +48,61 @@ public class PlayerUiController : MonoBehaviour
         }
 
         return stringBuilder.ToString();
+    }
+}
+
+
+public class WoodenThrowingKnife : Item
+{
+    public WoodenThrowingKnife(int count = 0)
+    {
+        Id = 1;
+        Name = "Throwing knife";
+        ResourcesNeededToCraft = new Dictionary<ResourceType, int>()
+        {
+            {ResourceType.Wood, 1}
+        };
+        Count = count;
+    }
+}
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public static Dictionary<ResourceType, int> ResourcesNeededToCraft { get; set; }
+    public int Damage { get; set; } = 1;
+    public int Count { get; set; }
+
+    public static int GetMaxNumberOfCraftsPossible(Dictionary<ResourceType, int> availableResources)
+    {
+        var maxCraftPerResource = new List<int>();
+        foreach (var resource in ResourcesNeededToCraft)
+        {
+            // If at least one of the needed resources is not available, we know 0 items can be crafted
+            if (!availableResources.ContainsKey(resource.Key))
+                return 0;
+
+            maxCraftPerResource.Add(availableResources[resource.Key] / resource.Value);
+        }
+
+        var lowestPossibleCraftCount = maxCraftPerResource.Min();
+        return lowestPossibleCraftCount;
+    }
+    
+    public static int Craft(Dictionary<ResourceType, int> resources)
+    {
+        foreach (var requirement in ResourcesNeededToCraft)
+        {
+            if(!resources.ContainsKey(requirement.Key))
+                throw new InvalidOperationException("Cant craft");
+
+            if(resources[requirement.Key] < requirement.Value)
+                throw new InvalidOperationException("Cant craft");
+            
+            resources[requirement.Key] -= requirement.Value;
+        }
+        
+        return 1;
     }
 }
