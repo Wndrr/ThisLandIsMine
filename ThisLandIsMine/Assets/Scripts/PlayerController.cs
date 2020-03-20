a236807473838183b993257d20b2f9fc3d5d63e4
Mathieu VIALES <mathieu@viales.fr>
2020-03-20 01:16:10 +0100

    Add player, bush, and associated controllers

---
+++ ThisLandIsMine/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float SpeedCoeficient;
+    private CharacterController _characterController;
+    public Canvas PlayerUi;
+    private int TotalRessource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        var targetPosition = new Vector2(x, y) * SpeedCoeficient;
+        _characterController.Move(targetPosition);
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
+
+            if (hit.collider != null)
+            {
+                if (hit.transform.CompareTag("collectable"))
+                {
+                    var ressource = hit.collider.GetComponentInParent<RessourceController>();
+                    var harvestedRessource = ressource.Harvest();
+                    var ressourceCount = PlayerUi.GetComponentsInChildren<Text>().First(t => t.name == "RessourceCount");
+                    TotalRessource += harvestedRessource;
+                    ressourceCount.text = $"Wood: {TotalRessource}";
+                }
+            }
+        }
+    }
+}
---
+++ ThisLandIsMine/Assets/Scripts/RessourceController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMesh))]
+public class RessourceController : MonoBehaviour
+{
+    public int RessourceCount = 10;
+    public int HarvestSpeed = 1;
+    public RessourceType RessourceType;
+    private TextMesh _text;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = gameObject.GetComponent<TextMesh>();
+    }
+
+    public int Harvest()
+    {
+        RessourceCount -= HarvestSpeed;
+        _text.text = RessourceCount.ToString();
+        
+        if(RessourceCount <= 0)
+            Destroy(gameObject);
+        
+        return HarvestSpeed;
+    }
+}
+
+public enum RessourceType
+{
+    Wood
+}
