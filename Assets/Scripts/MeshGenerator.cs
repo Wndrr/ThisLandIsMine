63efa2fc8bc3c5208354df46d2a4d822e7ed3880
Mathieu VIALES <mathieu@viales.fr>
2020-03-21 22:04:05 +0100

    Closes #3 - Create simplistic procedural 3D terrain generation

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour
+{
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    public int xSize = 20;
+    public int zSize = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        var meshFilter = GetComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+    }
+
+    private void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        var i = 0;
+        for (var z = 0; z <= zSize; z++)
+        {
+            for (var x = 0; x <= xSize; x++)
+            {
+                var y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+                vertices[i] = new Vector3(x, y, z) / 10;
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+        var vert = 0;
+        var tris = 0;
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+                vert++;
+                tris += 6;
+            }
+
+            vert++;
+        }
+    }
+
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+    }
+}
