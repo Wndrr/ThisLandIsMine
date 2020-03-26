554b446bf552743bd6007cd75de0c31e5b397167
Mathieu VIALES <mathieu@viales.fr>
2020-03-26 13:05:05 +0100

    Make inventory text look at player

---
+++ Assets/Scripts/Controllers/Ui/LookAtPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var player = GameObject.FindGameObjectsWithTag("Player").FirstOrDefault();
+        if (player == null)
+            return;
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
